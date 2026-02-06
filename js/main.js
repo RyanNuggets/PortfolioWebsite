bc08bc4099845679832d4e75b5ec2e1aea9e3aae
RyanNuggets <152134976+RyanNuggets@users.noreply.github.com>
2026-02-06 15:20:27 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,1 @@
-let t;
 
-window.addEventListener(
-  "scroll",
-  () => {
-    document.body.classList.add("is-scrolling");
-    clearTimeout(t);
-    t = setTimeout(() => document.body.classList.remove("is-scrolling"), 150);
-  },
-  { passive: true }
-);
