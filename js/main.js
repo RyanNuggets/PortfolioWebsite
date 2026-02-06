d17e028de83663160117302511db882c49d207ea
RyanNuggets <152134976+RyanNuggets@users.noreply.github.com>
2026-02-06 14:41:10 +0400

    Implement scroll event listener for body class
    Add scroll event listener to manage 'is-scrolling' class

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
 // reserved
+let t;
+
+window.addEventListener(
+  "scroll",
+  () => {
+    document.body.classList.add("is-scrolling");
+    clearTimeout(t);
+    t = setTimeout(() => document.body.classList.remove("is-scrolling"), 120);
+  },
+  { passive: true }
+);
